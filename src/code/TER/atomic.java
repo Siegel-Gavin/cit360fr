ea72938be89c420c31a579c490f09129f2c8e40f
Siegel-Gavin <gjwsiegel@gmail.com>
2019-06-22 17:41:54 -0400

    TER in progress

---
+++ src/code/TER/atomic.java
@@ -0,0 +1,57 @@
+package code.TER;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * this example is based off of the tutorial found here
+ * https://www.youtube.com/watch?v=IQSbIOKhC4g
+ */
+
+public class atomic {
+    private volatile int val = 0;
+    private int MAX = 30;
+
+
+    public void playPingPong() {
+        Thread t1 = new Thread(new runnable());
+        Thread t2 = new Thread(new runnable());
+        Thread t3 = new Thread(new runnable());
+
+        new Thread(() -> {
+            for (int lv = val; lv < MAX; )
+                if(lv != val) {
+                    System.out.println("pong(" + val + ")");
+                    lv = val;
+                }
+        }).start();
+
+        new Thread(() -> {
+            for (int lv = val; lv < MAX; ) {
+                val = ++lv;
+                System.out.println("ping(" + lv + ")");
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        }).start();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        executorService.execute(t1::start);
+        executorService.execute(t2::start);
+        executorService.execute(t3::start);
+
+
+
+
+
+
+    }
+
+}
---
+++ src/code/TER/runnable.java
@@ -0,0 +1,33 @@
+package code.TER;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * This code is based off of the tutorial found here
+ * https://examples.javacodegeeks.com/core-java/util/concurrent/atomic/atomicinteger/java-atomicinteger-example/
+ */
+
+public class runnable implements Runnable {
+    private static AtomicInteger at = new AtomicInteger(0);
+
+    @Override
+    public void run() {
+        int myCounter;
+        int myPrevCounter;
+        int myCounterPlusFive;
+        boolean isNine;
+        myCounter = at.incrementAndGet();
+        System.out.println("Thread " + Thread.currentThread().getId() + "  / Counter : " + myCounter);
+        myPrevCounter = at.getAndIncrement();
+        System.out.println("Thread " + Thread.currentThread().getId() + " / Previous : " + myPrevCounter);
+        myCounterPlusFive = at.addAndGet(5);
+        System.out.println("Thread " + Thread.currentThread().getId() + " / plus five : " + myCounterPlusFive);
+        isNine = at.compareAndSet(9, 3);
+        if (isNine) {
+            System.out.println("Thread " + Thread.currentThread().getId()
+                    + " / Value was equal to 9, so it was updated to " + at.intValue());
+
+        }
+
+
+    }
+}
